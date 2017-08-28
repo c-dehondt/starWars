44ee9810078d34e79aac9a387084940b22c9fcde
c.dehondt <c.dehondt@live.com>
2017-08-28 15:07:44 +0200

    all: add the files an folder for the project starWars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+// New imports to update based on AngularFire2 version 4
+import { FormsModule} from '@angular/forms';
+import{HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
